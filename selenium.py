e7ec2ac592e5e865e4b34964a127d481fe0090d1
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-09-10 11:08:12 -0400

    Update selenium.py

---
+++ selenium.py
@@ -0,0 +1,97 @@
+import re
+import time
+
+# The text containing email addresses
+text = """
+dylankrish@gmail.com
+dylan@dylankri.sh
+promotions@dylankri.sh
+krishnan.dylan@gmail.com
+dak5839@psu.edu
+"""
+
+# Regular expression pattern to match email addresses
+email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+# Find all email addresses in the text using the pattern
+email_addresses = re.findall(email_pattern, text)
+
+# Convert the list of email addresses to a Python list variable
+email_list = list(email_addresses)
+
+# Print the list of email addresses
+print(email_list)
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+
+subject = "Iowa Ticket"
+message = """
+Hello, 
+
+I am interested in buying your ticket. Please let me know if it is still available, and how low you are willing to sell it for.
+
+Thanks,
+Dylan Krishnan
+"""
+
+driver = webdriver.Chrome()
+
+driver.get("https://outlook.office365.com/mail/")
+driver.maximize_window()
+
+from credentials import username,password,totpsecret
+import pyotp
+import hashlib
+useTOTP = True
+
+# Login
+# Wait for login page
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.NAME, 'loginfmt')))
+# Type username
+driver.find_element(By.NAME, 'loginfmt').send_keys(username + '\n')
+
+# Send enter key
+# driver.find_element(By.NAME, 'loginfmt').send_keys(u'\ue007')
+# Wait for password page
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.NAME, 'passwd')))
+# Type password
+driver.find_element(By.NAME, 'passwd').send_keys(password)
+time.sleep(1)
+# Send enter key
+driver.find_element(By.NAME, 'passwd').send_keys(u'\ue007')
+if useTOTP:
+    # Wait for totp page
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.NAME, 'otc')))
+    # Create a TOTP object with the given secret
+    totp = pyotp.TOTP(totpsecret, digits=6, digest=hashlib.sha1)
+    # Get the current TOTP code
+    totpcode = totp.now()
+    # Type totp code
+    driver.find_element(By.NAME, 'otc').send_keys(totpcode)
+    # Send enter key
+    driver.find_element(By.NAME, 'otc').send_keys(u'\ue007')
+    # If the expected verification code is not entered, we need to try a different one
+    # check for id 'ViewDetails' or the login page
+    while True:
+        # if the view details button is visible, we are on the login page
+        if driver.find_elements(By.ID, 'ViewDetails'):
+            raise Exception('You didn\'t enter an expected verification code. Please try again.')
+        # if the view details button is not visible, we are on the mail page
+        elif driver.find_elements(By.ID, '_3rUUXFwqB4l2JguqIctQtd'):
+            break
+else:
+    # wait for the mail page
+    # TODO: send system notifications?
+    print('Please type your 2FA code and continue.')
+    while True:
+        if driver.find_elements(By.CLASS_NAME, '_3rUUXFwqB4l2JguqIctQtd'):
+            break
+
+# click id__174
+
+driver.find_element(By.ID, 'id__174').click()
